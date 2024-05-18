d09b86a8dcefda7f6c1a94b4ca34e788fac30e94
Bogdan Hritcu <bogdan.i.hritcu@gmail.com>
2024-05-18 23:15:16 +0300

    fixed concepts::de/serializer

--- include/flow/utility/serialization.hpp
+++ include/flow/utility/serialization.hpp
@@ -11,11 +11,11 @@ class ostream_view;
 
 namespace concepts {
 
-    template<typename T>
-    concept serializer = std::invocable<make_lval_ref_t<ostream_view>, make_const_lval_ref_t<T>>;
+    template<typename F, typename T>
+    concept serializer = std::invocable<F, make_lval_ref_t<ostream_view>, make_const_lval_ref_t<T>>;
 
-    template<typename T>
-    concept deserializer = std::invocable<make_lval_ref_t<istream_view>, make_lval_ref_t<T>>;
+    template<typename F, typename T>
+    concept deserializer = std::invocable<F, make_lval_ref_t<istream_view>, make_lval_ref_t<T>>;
 
 } // namespace concepts
 
