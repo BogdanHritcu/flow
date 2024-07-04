9a5ab3f877de6c9a475b645d7f3bb2db9b3978b6
Bogdan Hritcu <bogdan.i.hritcu@gmail.com>
2024-07-04 22:07:49 +0300

    minor refactoring to gl commands

--- include/flow/graphics/opengl/commands.hpp
+++ include/flow/graphics/opengl/commands.hpp
@@ -9,68 +9,62 @@
 
 #include "enum_types.hpp"
 
-namespace flow {
-namespace gl {
+namespace flow::gl {
 
-    inline void draw_arrays(primitive_type primitive, std::size_t count, std::size_t offset = 0) noexcept
-    {
-        glDrawArrays(static_cast<GLenum>(primitive), static_cast<GLint>(offset), static_cast<GLsizei>(count));
-    }
+inline void draw_arrays(primitive_type primitive, std::size_t count, std::size_t offset = 0) noexcept
+{
+    glDrawArrays(static_cast<GLenum>(primitive), static_cast<GLint>(offset), static_cast<GLsizei>(count));
+}
 
-    inline void draw_elements(primitive_type primitive, type_value type, std::size_t count, std::size_t offset = 0) noexcept
-    {
-        std::uintptr_t byte_offset = offset * sizeof_type_value(type);
+inline void draw_elements(primitive_type primitive, type_value type, std::size_t count, std::size_t offset = 0) noexcept
+{
+    glDrawElements(static_cast<GLenum>(primitive),
+                   static_cast<GLsizei>(count),
+                   static_cast<GLenum>(type),
+                   reinterpret_cast<void*>(static_cast<std::uintptr_t>(offset * sizeof_type_value(type)))); // NOLINT
+}
 
-        glDrawElements(static_cast<GLenum>(primitive),
-                       static_cast<GLsizei>(count),
-                       static_cast<GLenum>(type),
-                       reinterpret_cast<void*>(byte_offset));
-    }
+inline void draw_arrays_instanced(std::size_t instance_count, primitive_type primitive, std::size_t count, std::size_t offset = 0) noexcept
+{
+    glDrawArraysInstanced(static_cast<GLenum>(primitive),
+                          static_cast<GLint>(offset),
+                          static_cast<GLsizei>(count),
+                          static_cast<GLsizei>(instance_count));
+}
 
-    inline void draw_arrays_instanced(std::size_t instance_count, primitive_type primitive, std::size_t count, std::size_t offset = 0) noexcept
-    {
-        glDrawArraysInstanced(static_cast<GLenum>(primitive),
-                              static_cast<GLint>(offset),
-                              static_cast<GLsizei>(count),
-                              static_cast<GLsizei>(instance_count));
-    }
+inline void draw_elements_instanced(std::size_t instance_count, primitive_type primitive, type_value type, std::size_t count, std::size_t offset = 0) noexcept
+{
+    glDrawElementsInstanced(static_cast<GLenum>(primitive),
+                            static_cast<GLsizei>(count),
+                            static_cast<GLenum>(type),
+                            reinterpret_cast<void*>(static_cast<std::uintptr_t>(offset * sizeof_type_value(type))), // NOLINT
+                            static_cast<GLsizei>(instance_count));
+}
 
-    inline void draw_elements_instanced(std::size_t instance_count, primitive_type primitive, type_value type, std::size_t count, std::size_t offset = 0) noexcept
-    {
-        std::uintptr_t byte_offset = offset * sizeof_type_value(type);
+inline void clear(clear_target_flags flags = clear_target_flags::color) noexcept
+{
+    glClear(static_cast<GLbitfield>(flags));
+}
 
-        glDrawElementsInstanced(static_cast<GLenum>(primitive),
-                                static_cast<GLsizei>(count),
-                                static_cast<GLenum>(type),
-                                reinterpret_cast<void*>(byte_offset),
-                                static_cast<GLsizei>(instance_count));
-    }
+inline void set_clear_color(const glm::vec4& color) noexcept
+{
+    glClearColor(static_cast<GLfloat>(color.r),
+                 static_cast<GLfloat>(color.g),
+                 static_cast<GLfloat>(color.b),
+                 static_cast<GLfloat>(color.a));
+}
 
-    inline void clear(clear_target_flags flags = clear_target_flags::color) noexcept
-    {
-        glClear(static_cast<GLbitfield>(flags));
-    }
+inline void set_viewport(glm::uvec2 start, glm::uvec2 size) noexcept
+{
+    glViewport(static_cast<GLint>(start.x),
+               static_cast<GLint>(start.y),
+               static_cast<GLint>(size.x),
+               static_cast<GLint>(size.y));
+}
 
-    inline void set_clear_color(const glm::vec4& color) noexcept
-    {
-        glClearColor(static_cast<GLfloat>(color.r),
-                     static_cast<GLfloat>(color.g),
-                     static_cast<GLfloat>(color.b),
-                     static_cast<GLfloat>(color.a));
-    }
+inline void set_polygon_mode(polygon_mode mode) noexcept
+{
+    glPolygonMode(GL_FRONT_AND_BACK, static_cast<GLenum>(mode));
+}
 
-    inline void set_viewport(glm::uvec2 start, glm::uvec2 size) noexcept
-    {
-        glViewport(static_cast<GLint>(start.x),
-                   static_cast<GLint>(start.y),
-                   static_cast<GLint>(size.x),
-                   static_cast<GLint>(size.y));
-    }
-
-    inline void set_polygon_mode(polygon_mode mode) noexcept
-    {
-        glPolygonMode(GL_FRONT_AND_BACK, static_cast<GLenum>(mode));
-    }
-
-} // namespace gl
-} // namespace flow
+} // namespace flow::gl
