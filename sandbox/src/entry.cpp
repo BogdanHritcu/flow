130bd474b923593d8dba0016e6a1cf20c793e028
Bogdan Hritcu <bogdan.i.hritcu@gmail.com>
2024-07-17 00:28:24 +0300

    changed active test to rectangle_renderer_test

--- sandbox/src/entry.cpp
+++ sandbox/src/entry.cpp
@@ -2,18 +2,14 @@
 
 #include <memory>
 
-// #include "tests/dense_tree_test.hpp"
-// #include "tests/input_system_test.hpp"
-// #include "tests/textured_quads_test.hpp"
-// #include "tests/utility_test.hpp"
-// #include "tests/physics_test.hpp"
-#include "tests/renderer_line_test.hpp"
+// #include "tests/line_renderer_test.hpp"
+#include "tests/rectangle_renderer_test.hpp"
 
 namespace flow {
 
 std::unique_ptr<application> entry(int /*argc*/, char** /*argv*/)
 {
-    return std::make_unique<renderer_line_test>();
+    return std::make_unique<rectangle_renderer_test>();
 }
 
 } // namespace flow
