1bfcc8585c585abca2e17bf0d458e2a49e3a04fe
Bogdan Hritcu <bogdan.i.hritcu@gmail.com>
2024-09-01 17:20:49 +0300

    added a linear curve and changed the names of the other curves

--- include/flow/utility/curve.hpp
+++ include/flow/utility/curve.hpp
@@ -7,16 +7,57 @@
 namespace flow {
 
 template<std::floating_point T>
-class basic_quadratic_bezier
+class basic_linear_curve
 {
 public:
     using value_type = T;
     using vec2_type = flow::basic_vec2<T>;
 
 public:
-    constexpr basic_quadratic_bezier() noexcept = default;
+    constexpr basic_linear_curve() noexcept = default;
 
-    constexpr basic_quadratic_bezier(const vec2_type b0, const vec2_type b1, const vec2_type b2) noexcept
+    constexpr basic_linear_curve(const concepts::vector_least2<value_type> auto& b0,
+                                 const concepts::vector_least2<value_type> auto& b1) noexcept
+        : m_b0{ b0.x, b0.y }
+        , m_b1{ b1.x, b1.y }
+    {
+    }
+
+    void set_points(const concepts::vector_least2<value_type> auto& b0,
+                    const concepts::vector_least2<value_type> auto& b1) noexcept
+    {
+        m_b0 = { b0.x, b0.y };
+        m_b1 = { b1.x, b1.y };
+    }
+
+    template<concepts::vector_least2<value_type> VecT = vec2_type>
+    [[nodiscard]] constexpr VecT evaluate(value_type t) const noexcept
+    {
+        return (value_type{ 1 } - t) * VecT{ m_b0.x, m_b0.y } + t * VecT{ m_b1.x, m_b1.y };
+    }
+
+    template<concepts::vector_least2<value_type> VecT = vec2_type>
+    [[nodiscard]] constexpr decltype(auto) operator()(value_type t) const noexcept
+    {
+        return evaluate<VecT>(t);
+    }
+
+private:
+    vec2_type m_b0{};
+    vec2_type m_b1{};
+};
+
+template<std::floating_point T>
+class basic_quadratic_bezier_curve
+{
+public:
+    using value_type = T;
+    using vec2_type = flow::basic_vec2<T>;
+
+public:
+    constexpr basic_quadratic_bezier_curve() noexcept = default;
+
+    constexpr basic_quadratic_bezier_curve(const vec2_type b0, const vec2_type b1, const vec2_type b2) noexcept
         : m_b0{ b0 }
         , m_b1{ b1 }
         , m_b2{ b2 }
@@ -51,16 +92,16 @@ private:
 };
 
 template<std::floating_point T>
-class basic_cubic_bezier
+class basic_cubic_bezier_curve
 {
 public:
     using value_type = T;
     using vec2_type = flow::basic_vec2<T>;
 
 public:
-    constexpr basic_cubic_bezier() noexcept = default;
+    constexpr basic_cubic_bezier_curve() noexcept = default;
 
-    constexpr basic_cubic_bezier(const vec2_type b0, const vec2_type b1, const vec2_type b2, const vec2_type b3) noexcept
+    constexpr basic_cubic_bezier_curve(const vec2_type b0, const vec2_type b1, const vec2_type b2, const vec2_type b3) noexcept
         : m_b0{ b0 }
         , m_b1{ b1 }
         , m_b2{ b2 }
@@ -99,7 +140,8 @@ private:
     vec2_type m_b3{};
 };
 
-using quadratic_bezier = basic_quadratic_bezier<float>;
-using cubic_bezier = basic_cubic_bezier<float>;
+using linear_curve = basic_linear_curve<float>;
+using quadratic_bezier_curve = basic_quadratic_bezier_curve<float>;
+using cubic_bezier_curve = basic_cubic_bezier_curve<float>;
 
 } // namespace flow
